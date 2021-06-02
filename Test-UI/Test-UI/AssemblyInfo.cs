ac4bb5c50dc287c3124834bf289d5158e7e72ddf
1 <1>
2021-06-02 20:16:11 +0200

    update

--- Test-UI/Test-UI/AssemblyInfo.cs
+++ Test-UI/Test-UI/AssemblyInfo.cs
@@ -3,4 +3,3 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("pomidorko-regular.ttf", Alias = "pomidorko-regular")]
-[assembly: ExportFont("goodwine-normal.otf", Alias = "goodwine-normal")]
